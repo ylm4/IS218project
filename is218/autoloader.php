388c43d3b8eccae220fe3cc229175fe29d10040c
Yanira Morales <ylm4@njit.edu>
2016-12-18 22:29:59 -0500

    fixed

--- is218/autoloader.php
+++ is218/autoloader.php
@@ -4,7 +4,7 @@ class Autoloader
 	public static function loader($class)
 	{
 		$filename= strtolower($class) . '.php';
-		$file= 'is218/' . $filename;
+		$file= 'IS218project/is218/' . $filename;
 		if (!file_exists($file))
 		{
 			return false;
